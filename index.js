3aa73c79ea86ba2228eb8744416f53d065389b8a
Derek <derekylee@outlook.com>
2022-12-10 22:32:59 -0800

    github oauth

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const logger = require('morgan')
 const express = require('express')
+const axios = require('axios')
 const cors = require('cors')
 const app = express()
 const router = require('./router')
+
+const passport = require('passport')
+const GithubStrategy = require('passport-github').Strategy;
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_URL = 'https://github.com/login/oauth/access_token';
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
@@ -12,9 +21,28 @@ app.use(logger('tiny'));
 app.use(express.json());
 app.use(cors());
 
+// passport.use(new GithubStrategy({
+//   clientID: GITHUB_CLIENT_ID,
+//   clientSecret: GITHUB_CLIENT_SECRET,
+//   callbackURL: 'http://localhost:5127'
+// },
+//   function (accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ githubId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
 app.post('/register', router.register);
 app.post('/login', router.login);
 app.post('/edit-profile', router.editProfile);
+
+app.get('/login/federated/github', passport.authenticate('github'));
+app.get('/oauth/redirect', router.oauthGithub);
+
+
+
+
 
 app.use((err, req, res, next) => {
   console.error(err);
@@ -26,7 +54,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(
     `Express Server started on Port ${app.get('port')} |
     Environment : ${app.get('env')}`
--- router.js
+++ router.js
@@ -1,23 +1,37 @@
+const axios = require('axios');
 const { MongoClient } = require('mongodb')
+
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const dbName = 'authyDB';
+const db = client.db(dbName);
+const users = db.collection('users');
 
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_URL = 'https://github.com/login/oauth/access_token';
 
-// const main = async() => {
+// const registerOauthGeneric = async (req, res, userObject) => {
 //   await client.connect();
 //   const db = client.db(dbName);
 //   const users = db.collection('users');
-//   const findResult = await users.find({}).toArray();
-//   return findResult
+//   try {
+//     await users.insertOne(userObject);
+//     res.redirect('http://localhost:5173')
+//   }
+//   catch (err) {
+//     console.log('error occurred:', err);
+//     res.status(500).send('an error has occurred');
+//   }
+//   finally {
+//     client.close()
+//   }
 // }
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
   await client.connect();
-  const db = client.db(dbName);
-  const users = db.collection('users');
   try {
     const findResult = await users.find({ email: email }).toArray();
     if (!findResult[0]) {
@@ -40,8 +54,6 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   await client.connect();
-  const db = client.db(dbName);
-  const users = db.collection('users');
   try {
     const findResult = await users.find({ email: email, password: password }).toArray();
     if (findResult[0]) {
@@ -68,18 +80,90 @@ const login = async (req, res, next) => {
 }
 
 const editProfile = async (req, res, next) => {
-  const { curr_email, name, bio, phone, email, password, picture_url } = req.body;
-
+  const { oauth_id, curr_email, name, bio, phone, email, password, picture_url } = req.body;
   await client.connect();
-  const db = client.db(dbName);
-  const users = db.collection('users');
   try {
-    const userResults = await users.findOneAndUpdate(
-      { email: curr_email },
-      { $set: { name: name, bio: bio, phone: phone, email: email, password: password, picture_url: picture_url } },
-      { returnDocument: 'after', upsert: true }
-    );
-    res.json(userResults);
+    if (oauth_id) {
+      const userResults = await users.findOneAndUpdate(
+        { oauth_id: oauth_id },
+        { $set: { name: name, bio: bio, phone: phone, email: email, password: password, picture_url: picture_url } },
+        { returnDocument: 'after', upsert: true }
+      );
+      res.json(userResults);
+    }
+    else {
+      const userResults = await users.findOneAndUpdate(
+        { email: curr_email },
+        { $set: { name: name, bio: bio, phone: phone, email: email, password: password, picture_url: picture_url } },
+        { returnDocument: 'after', upsert: true }
+      );
+      res.json(userResults);
+    }
+  }
+  catch (err) {
+    console.log('error occurred:', err);
+    res.status(500).send('an error has occurred');
+  }
+  finally {
+    client.close()
+  }
+}
+
+const oauthGithub = async (req, res, next) => {
+  const access_token = await axios.post(`${GITHUB_URL}?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${req.query.code}`,
+    {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+  )
+    .then((response) => {
+      if (typeof response.data != 'string') {
+        throw 'github actually returned a json format, update ur code'
+      }
+      const token = response.data.slice(
+        response.data.indexOf('=') + 1, response.data.indexOf('&')
+      )
+      return token
+    })
+    .catch((err) => {
+      console.log('error:', err);
+      res.status(500).send('an error has occurred');
+    })
+  // console.log('token ready:', access_token);
+  const user_data = await axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    }
+  })
+    .then((response) => {
+      // console.log(response.data)
+      const newGithubUser = {
+        oauth_id: `github=${response.data.id}`,
+        email: '',
+        password: '',
+        bio: response.data.bio,
+        name: response.data.name,
+        phone: '',
+        picture_url: response.data.avatar_url,
+      }
+      return newGithubUser
+    })
+    .catch((err) => {
+      console.log('error:', err);
+      res.status(500).send('an error has occurred');
+    })
+  // console.log('github user data:', user_data);
+  try {
+    await client.connect();
+    const findResult = await users.find({ oauth_id: user_data.oauth_id }).toArray();
+    if (!findResult[0]) {
+      await users.insertOne(user_data);
+      res.json(user_data);
+    }
+    else {
+      res.json(findResult[0]);
+    }
   }
   catch (err) {
     console.log('error occurred:', err);
@@ -93,5 +177,6 @@ const editProfile = async (req, res, next) => {
 module.exports = {
   register,
   login,
-  editProfile
+  editProfile,
+  oauthGithub,
 }
